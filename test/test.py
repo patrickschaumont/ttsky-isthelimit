2bc220e05fe60d87f1d58a8f4ad258dfa51ade96
Patrick Schaumont <patrickschaumont@users.noreply.github.com>
2025-08-20 14:13:47 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -29,7 +29,7 @@ async def test_project(dut):
     dut.ui_in.value = 1
     dut.uio_in.value = 0
 
-    await ClockCycles(dut.clk, 256)
+    await ClockCycles(dut.clk, 257)
     assert dut.uo_out.value == 1
 
     await ClockCycles(dut.clk, 256)
